3295231475c01d2f8c7a5213de84489b8f457528
bochain <bochainwu@gmail.com>
2023-09-28 23:21:09 +0800

    update week3_practice

--- week3_practice/main.c
+++ week3_practice/main.c
@@ -24,27 +24,27 @@ int main(int argc, char *argv[]) {
 	};        
 	struct sam s[100];
 	
-	int i, j, k, ll, m, n;
+	int i, j, k, l, m, n;
 	//int *psx;
 	//char *pc;
 	//float *pf;
 	//double *pd, *psy;
 	//struct sam *pstart;
-	int *p0, *p1, *p2, *p3, *pa, *p5, *p6;
+	int *p0, *p1, *p2, *p3, *p4, *p5, *p6;
 	
 	i = sizeof(int); //char = 1
 	j = sizeof(char);//float = 4
 	k = sizeof(float);// dobule = 8
 	m = sizeof(double); //unicon =16 (Structure Member Alignment)
-	ll = sizeof(struct stfloat);//int = 4*3 =>12
+	l = sizeof(struct stfloat);//int = 4*3 =>12
 	n = sizeof(struct sam); //40 => 1+ 4*2 + 8 + 16 = 33 (Structure Member Alignment)
-	
-	p0 = &(s[0]); //0x62ee30 starting address
-	p1 = &(s[17].c1);//0x62f0e0 = 0x62ee30 + 0x200 (40*17+8=688)
-	p2 = &(s[13].f6);//0x62f044 = 0x62ee30 + 0x214 (40*13+8+4=532)
-	p3 = &(s[28].f8);//0x62f290 = 0x62ee30 + 0x460 (40*28=1120)
-	pa = &(s[17].f13.f11.f3);//0x62f0f0 = 0x62ee30 + 0x2c0 (40*17+8+4+4+(4+4) = 704)
-	p5 = &(s[27].f13.f12.f5);//0x62f280 = 0x62ee30 +0x450 (40*27+8+4+4+(8)=1104)
+	//411440430 bo-chain
+	p0 = &(s[0]);
+	p1 = &(s[17].c1);
+	p2 = &(s[13].f6);
+	p3 = &(s[28].f8);
+	p4 = &(s[17].f13.f11.f3);
+	p5 = &(s[27].f13.f12.f5);
 	//pstart = &(s[0]);
 	//pc = &(s[17].c1);
 	//pf = &(s[13].f6);
